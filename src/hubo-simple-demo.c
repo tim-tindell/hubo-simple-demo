a547245a31a302eea075d2f72c52ff6524eadc79
tim-tindell <rtindel2@gmu.edu>
2014-09-06 21:05:03 -0400

    Move Leg up and other leg oscillates up and down

--- src/hubo-simple-demo.c
+++ src/hubo-simple-demo.c
@@ -22,16 +22,37 @@
 ach_channel_t chan_hubo_ref;      // Feed-Forward (Reference)
 ach_channel_t chan_hubo_state;    // Feed-Back (State)
 
-int main(int argc, char **argv) {
+//Sleep with respect to Sim Time
+void wsleep(double ts,double runtime,struct hubo_state Hs){
+   double t;
+   t=ts-runtime;
+   size_t fs;
+   //Get Current Sim Time
+   int r = ach_get( &chan_hubo_state, &Hs, sizeof(Hs), &fs, NULL, ACH_O_WAIT );
+       if(ACH_OK != r) {
+          assert( sizeof(Hs) == fs );
+       }
+   t+=Hs.time;
+   while(Hs.time<t){
+      
+       /* Get the current feed-forward (state) */
+       r = ach_get( &chan_hubo_state, &Hs, sizeof(Hs), &fs, NULL, ACH_O_WAIT );
+       if(ACH_OK != r) {
+          assert( sizeof(Hs) == fs );
+       }
+       //printf("%f<%f\r\n",Hs.time,t);
+}
+return;
+}
+	
 
+int main(int argc, char **argv) {
     /* Open Ach Channel */
     int r = ach_open(&chan_hubo_ref, HUBO_CHAN_REF_NAME , NULL);
     assert( ACH_OK == r );
 
     r = ach_open(&chan_hubo_state, HUBO_CHAN_STATE_NAME , NULL);
     assert( ACH_OK == r );
-
-
 
     /* Create initial structures to read and write from */
     struct hubo_ref H_ref;
@@ -41,27 +62,80 @@ int main(int argc, char **argv) {
 
     /* for size check */
     size_t fs;
+    /* Move the hips to move the center of gravity over one foot*/
+    
+    double amount=0;
+    while(amount<0.18){
+       H_ref.ref[LHR] = amount;
+       H_ref.ref[RHR] = amount;
+       H_ref.ref[LAR] = -amount;
+       H_ref.ref[RAR] = -amount;
 
-    /* Get the current feed-forward (state) */
-    r = ach_get( &chan_hubo_state, &H_state, sizeof(H_state), &fs, NULL, ACH_O_LAST );
-    if(ACH_OK != r) {
-        assert( sizeof(H_state) == fs );
+       /* Write to the feed-forward channel */
+       ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+       usleep(1000000);
+       amount+=0.025;
     }
-
-    /* Set Left Elbow Bend (LEB) and Right Shoulder Pitch (RSP) to  -0.2 rad and 0.1 rad respectively*/
-    H_ref.ref[LEB] = -0.2;
-    H_ref.ref[RSP] = 0.1;
-
-    /* Print out the actual position of the LEB */
-    double posLEB = H_state.joint[LEB].pos;
-    printf("Joint = %f\r\n",posLEB);
-
-    /* Print out the Left foot torque in X */
-    double mxLeftFT = H_state.ft[HUBO_FT_L_FOOT].m_x;
-    printf("Mx = %f\r\n", mxLeftFT);
-
-    /* Write to the feed-forward channel */
+    printf("Finished Hip Movement\n");
+    /*Move the left leg into the air*/
+    while(amount<0.55){
+       H_ref.ref[LHP] = -amount;
+       H_ref.ref[LKN] = 2*amount;
+       H_ref.ref[LAP] = -amount;
+       /* Write to the feed-forward channel */
+       ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+       usleep(1000000);
+       amount+=0.025;
+    }
+    printf("Finished Putting Leg in the Air\n");
     ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
 
+    /*bend right knee and ankle sinusoidally*/
+    int x=0;
+    amount=0.05;
+    while(1){
+       /* Get the current feed-forward (state) */
+       r = ach_get( &chan_hubo_state, &H_state, sizeof(H_state), &fs, NULL, ACH_O_LAST );
+       if(ACH_OK != r) {
+           assert( sizeof(H_state) == fs );
+       }
+       double before_move=H_state.time;
+       //Go Down
+       if(x==0){
+          H_ref.ref[RHP] = -amount;
+          H_ref.ref[RKN] = 2*amount;
+          H_ref.ref[RAP] = -amount;
+          if(amount>=0.4){
+	      x=1;
+      	      printf("Went Down\r\n");
+         }
+	 else{
+            amount=amount+0.05;
+	 }
+      }
+      //Go  Up
+      else{
+          H_ref.ref[RHP] =-amount;
+          H_ref.ref[RKN] = 2*amount;
+          H_ref.ref[RAP] = -amount;
+  	  if(amount<=0.05){
+              x=0;
+              printf("Went Up\r\n");
+          }
+	  else{
+             amount=amount-0.05;
+	  }
+      }
+       /* Write to the feed-forward channel */
+       ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+             /* Get the current feed-forward (state) */
+       r = ach_get( &chan_hubo_state, &H_state, sizeof(H_state), &fs, NULL, ACH_O_LAST );
+       if(ACH_OK != r) {
+           assert( sizeof(H_state) == fs );
+       }
+      double runtime=H_state.time-before_move;
+      wsleep(0.0625,runtime,H_state);
+    
+   }
+    
 }
-
