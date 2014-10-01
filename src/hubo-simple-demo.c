22165b2624ccc6b7166ba2ffba9835262858f548
tim-tindell <rtindel2@gmu.edu>
2014-09-30 20:57:13 -0400

    Midterm1 changes
    Hubo now will stand on one leg and oscillate twice, then stand the other leg and oscillate twice

--- src/hubo-simple-demo.c
+++ src/hubo-simple-demo.c
@@ -45,7 +45,7 @@ void wsleep(double ts,double runtime,struct hubo_state Hs){
 return;
 }
 	
-
+/*=======================Begin main function=======================*/
 int main(int argc, char **argv) {
     /* Open Ach Channel */
     int r = ach_open(&chan_hubo_ref, HUBO_CHAN_REF_NAME , NULL);
@@ -62,8 +62,8 @@ int main(int argc, char **argv) {
 
     /* for size check */
     size_t fs;
-    /* Move the hips to move the center of gravity over one foot*/
-    
+
+/* Move the hips to move the center of gravity over right foot*/
     double amount=0;
     while(amount<0.2){
        H_ref.ref[LHR] = amount;
@@ -77,9 +77,11 @@ int main(int argc, char **argv) {
        amount+=0.025;
     }
     sleep(5);
-    printf("Finished Hip Movement\n");
+    printf("Finished Hip Movement to right leg\n");
+    
 
-    /*Move the left leg into the air*/
+/*Move the left leg into the air*/
+    amount=0;
     while(amount<0.8){
        H_ref.ref[LHP] = -amount;
        H_ref.ref[LKN] = 2*amount;
@@ -90,13 +92,14 @@ int main(int argc, char **argv) {
        amount+=0.025;
     }
     sleep(15);
-    printf("Finished Putting Leg in the Air\n");
+    printf("Finished Putting left Leg in the Air\n");
     ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
 
-    /*bend right knee and ankle sinusoidally*/
+/*bend right knee and ankle sinusoidally twice*/
     int x=0;
     amount=0.05;
-    while(1){
+    int count=0;
+    while(count!=2){
        /* Get the current feed-forward (state) */
        r = ach_get( &chan_hubo_state, &H_state, sizeof(H_state), &fs, NULL, ACH_O_LAST );
        if(ACH_OK != r) {
@@ -108,7 +111,7 @@ int main(int argc, char **argv) {
           H_ref.ref[RHP] = -amount;
           H_ref.ref[RKN] = 2*amount;
           H_ref.ref[RAP] = -amount;
-          if(amount>=0.675){
+          if(amount>=0.75){
 	      x=1;
       	      printf("Went Down\r\n");
          }
@@ -123,6 +126,7 @@ int main(int argc, char **argv) {
           H_ref.ref[RAP] = -amount;
   	  if(amount<=0.05){
               x=0;
+	      count++;
               printf("Went Up\r\n");
           }
 	  else{
@@ -140,5 +144,130 @@ int main(int argc, char **argv) {
       wsleep(0.0625,runtime,H_state);
     
    }
+   printf("Right leg got its work out\n");
+   usleep(1000000);
+
+/* Put leg back down*/
+   amount=0.8;
+   while(amount>0){
+       H_ref.ref[LHP] = -amount;
+       H_ref.ref[LKN] = 2*amount;
+       H_ref.ref[LAP] = -amount;
+       /* Write to the feed-forward channel */
+       ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+       usleep(2500000);
+       amount-=0.025;
+    }
+   printf("left leg is back down\n");
+
+/* Move the hips to move the center of gravity over left foot*/ 
+    amount=0.2;
+    while(amount>-0.2){
+       H_ref.ref[LHR] = amount;
+       H_ref.ref[RHR] = amount;
+       H_ref.ref[LAR] = -amount;
+       H_ref.ref[RAR] = -amount;
+
+       /* Write to the feed-forward channel */
+       ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+       usleep(1000000);
+       amount-=0.025;
+    }
+    sleep(5);
+    printf("Finished Hip Movement to left leg\n");
+
+/*Move the right leg into the air*/
+    amount=0;
+    while(amount<0.5){
+       H_ref.ref[RHP] = -amount;
+       H_ref.ref[RKN] = 2*amount;
+       H_ref.ref[RAP] = -amount;
+       /* Write to the feed-forward channel */
+       ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+       usleep(2500000);
+       amount+=0.025;
+    }
+    sleep(15);
+    printf("Finished Putting right Leg in the Air\n");
+    ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+
+/*bend left knee and ankle sinusoidally*/
+    x=0;
+    amount=0.05;
+    count=0;
+    while(count!=2){
+       /* Get the current feed-forward (state) */
+       r = ach_get( &chan_hubo_state, &H_state, sizeof(H_state), &fs, NULL, ACH_O_LAST );
+       if(ACH_OK != r) {
+           assert( sizeof(H_state) == fs );
+       }
+       double before_move=H_state.time;
+       //Go Down
+       if(x==0){
+          H_ref.ref[LHP] = -amount;
+          H_ref.ref[LKN] = 2*amount;
+          H_ref.ref[LAP] = -amount;
+          if(amount>=0.4){
+	      x=1;
+      	      printf("Went Down\r\n");
+         }
+	 else{
+            amount=amount+0.025;
+	 }
+      }
+      //Go  Up
+      else{
+          H_ref.ref[LHP] =-amount;
+          H_ref.ref[LKN] = 2*amount;
+          H_ref.ref[LAP] = -amount;
+  	  if(amount<=0.05){
+              x=0;
+	      count++;
+              printf("Went Up\r\n");
+          }
+	  else{
+             amount=amount-0.025;
+	  }
+      }
+       /* Write to the feed-forward channel */
+       ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+             /* Get the current feed-forward (state) */
+       r = ach_get( &chan_hubo_state, &H_state, sizeof(H_state), &fs, NULL, ACH_O_LAST );
+       if(ACH_OK != r) {
+           assert( sizeof(H_state) == fs );
+       }
+      double runtime=H_state.time-before_move;
+      wsleep(0.0625,runtime,H_state);
     
+   }
+   printf("Right leg got its work out\n");
+   usleep(1000000);
+
+/* Put leg back down*/
+   amount=0.5;
+   while(amount>0){
+       H_ref.ref[RHP] = -amount;
+       H_ref.ref[RKN] = 2*amount;
+       H_ref.ref[RAP] = -amount;
+       /* Write to the feed-forward channel */
+       ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+       usleep(2500000);
+       amount-=0.025;
+    }
+   printf("left leg is back down\n");
+
+/*Move hips to 0*/
+    amount=-0.2;
+    while(amount<0){
+       H_ref.ref[LHR] = amount;
+       H_ref.ref[RHR] = amount;
+       H_ref.ref[LAR] = -amount;
+       H_ref.ref[RAR] = -amount;
+
+       /* Write to the feed-forward channel */
+       ach_put( &chan_hubo_ref, &H_ref, sizeof(H_ref));
+       usleep(1000000);
+       amount+=0.025;
+    }
+    printf("Ah I am done!! Great leg work out\n");
 }
